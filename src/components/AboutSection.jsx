cf39df635b816e3928e35829b91fe36543cc55b0
simonlunay <simonlunay@gmail.com>
2026-09-15 16:58:47 -0400

    new resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -23,8 +23,8 @@ export const AboutSection = () => {
                         <a href="#contact" className="cosmic-button">
                             Get In Touch
                         </a>
-                        <a href="/projects/Simon_Lunay_CV.pdf" download="Simon_Lunay_CV.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            Download CV
+                        <a href="/projects/Simon_Lunay_Resume.pdf" download="Simon_Lunay_Resume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            Download Resume
                         </a>
                     </div>
                 </div>
